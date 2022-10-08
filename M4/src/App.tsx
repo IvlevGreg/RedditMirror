fd2ed4bd2f95e95a844d9d92046f555dbb3988b9
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2022-10-08 19:48:50 +0300

    problems with video 4.5

--- M4/src/App.tsx
+++ M4/src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import { hot } from 'react-hot-loader/root'
 import styles from './app.css'
+import { Content } from "./shared/Content"
+import { Header } from "./shared/Header"
+import { Layout } from "./shared/Layout"
 
 function AppComponent() {
   return (
-    <div className={styles.title}>content</div>
+   <Layout >
+    <Header />
+    <Content>
+      content
+    </Content>
+   </Layout>
+
   )
 }
 
