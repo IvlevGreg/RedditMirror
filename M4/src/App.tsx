8b292e2a3f76011d20277cedab08fc67ed42724d
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2022-10-11 13:04:22 +0300

    deliberately wrong global css import

--- M4/src/App.tsx
+++ M4/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import styles from './app.css';
+import stylesGlobal from './main.global.css';
 import { CardsList } from './shared/CardsList';
 import { Content } from './shared/Content';
 import { Header } from './shared/Header';
@@ -9,6 +10,8 @@ import { Layout } from './shared/Layout';
 function AppComponent() {
   return (
     <Layout>
+      {/* удалить потом подключение */}
+      <div className={stylesGlobal.w}></div>
       <Header />
       <Content>
         <CardsList />
