d78f676514629610103cb3a4fcd499138f3a2629
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2022-10-25 10:05:29 +0300

    complete

---
+++ M7/src/shared/CardsList/Card/Menu/Menu.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Dropdown } from '../../../Dropdown';
+import { MenuIcon } from '../../../Icons';
+import styles from './menu.css';
+import { MenuItemsList } from './MenuItemsList';
+
+export function Menu() {
+  return (
+    <div className={styles.menu}>
+      <Dropdown
+        onClose={() => console.log('closed')}
+        onOpen={() => console.log('Open')}
+        isOpen={false}
+        button={
+          <button className={styles.menuButton}>
+            <MenuIcon />
+          </button>
+        }
+      >
+        <div className={styles.dropdown}>
+          <MenuItemsList />
+          <button className={styles.closeButton}>Закрыть</button>
+        </div>
+      </Dropdown>
+    </div>
+  );
+}
