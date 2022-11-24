df6f49afd13e223f728d3e32895fff2906be00b6
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2022-11-24 13:28:21 +0300

    add cors

--- M15/src/server/server.js
+++ M15/src/server/server.js
@@ -4,21 +4,46 @@ import { App } from '../App.tsx';
 import { indexTemplate } from './indexTemplate.js';
 import compression from 'compression';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
 const IS_DEV = process.env.NODE_ENV !== 'production';
 
 const app = express();
+app.use(cors());
 
-// if (!IS_DEV) {
-//   app.use(compression());
-//   app.use(
-//     helmet({
-//       crossOriginEmbedderPolicy: false,
-//       contentSecurityPolicy: false,
-//     })
-//   );
-// }
+if (!IS_DEV) {
+  app.use(compression());
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: false,
+    })
+  );
+}
+
+var allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  `http://localhost:${PORT}`,
+  'https://react-app-reddit-mirror.herokuapp.com',
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use('/static', express.static('./dist/client'));
 
